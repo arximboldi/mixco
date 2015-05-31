arximboldi/mixco

commit c445abbcde66d2a867ba66e3e6a66bb7968c57cb

Generate test coverage for the CoffeeScript code
Before we generated coverage for the JavaScript generated, which is not
that useful.  Now we generate coverage for the original Coffee sources.

First, we had to change our testing framework to Mocha + Chai + Sinon.
This is so because coffee-istanbul does not work with jasmine-node.

Also, to avoid generating coverage for the generated code, we had to change
the imports in the example scripts to include directly the Coffee sources.
While this makes them slightly less portable and less useful as an
example, this is probably alright.

makefile
@@ -13,7 +13,7 @@ NODEJS     = node
 COFFEE     = $(NODE_BIN)/coffee
 BROWSERIFY = $(NODE_BIN)/browserify
 DOCCO      = $(NODE_BIN)/docco
-JASMINE    = $(NODE_BIN)/jasmine-node
+MOCHA      = $(NODE_BIN)/_mocha
 ISTANBUL   = $(NODE_BIN)/istanbul
 
 SCRIPTS    = \
@@ -61,6 +61,7 @@ doc: $(DOCS)
 	cp -r ./pic ./doc/
 
 .SECONDARY:
+.PHONY: test
 
 lib/%.js: src/%.litcoffee
 	@mkdir -p $(@D)
@@ -124,10 +125,14 @@ clean:
 	find . -name "*~" -exec rm -f {} \;
 
 test:
-	NODE_PATH="$(NODE_PATH):.." $(JASMINE) spec --verbose --coffee
+	$(MOCHA) \
+		--recursive --compilers coffee:coffee-script/register
 
 test-coverage:
-	NODE_PATH="$(NODE_PATH):.." $(ISTANBUL) cover --root ./lib $(JASMINE) spec -- --verbose --coffee
+	$(ISTANBUL) cover $(MOCHA) -- \
+		--recursive --compilers coffee:coffee-script/register \
+		--require coffee-coverage/register-istanbul
+	$(ISTANBUL) report text lcov
 
 upload-doc: doc
 	ncftpput -R -m -u u48595320 sinusoid.es /mixco doc/*

script/novation_twitch.js
@@ -30,10 +30,10 @@
 // framework.
 
 var _      = require('underscore')
-var script = require('mixco/lib/script')
-var c      = require('mixco/lib/control')
-var b      = require('mixco/lib/behaviour')
-var v      = require('mixco/lib/value')
+var script = require('../src/script')
+var c      = require('../src/control')
+var b      = require('../src/behaviour')
+var v      = require('../src/value')
 
 // The script
 // ----------

script/korg_nanokontrol2.litcoffee
@@ -19,11 +19,11 @@ using the [NodeJS,
 **require**](http://nodejs.org/api/modules.html#modules_module_require_id)
 function.
 
-    {assert}  = require 'mixco/lib/util'
-    script    = require 'mixco/lib/script'
-    control   = require 'mixco/lib/control'
-    behaviour = require 'mixco/lib/behaviour'
-    value     = require 'mixco/lib/value'
+    {assert}  = require '../src/util'
+    script    = require '../src/script'
+    control   = require '../src/control'
+    behaviour = require '../src/behaviour'
+    value     = require '../src/value'
 
 Also, lets define these couple of shortcuts to save typing.
 

script/maudio_xponent.litcoffee
@@ -8,12 +8,12 @@ functionallity of the controls.
 
   ![Xponent Layout](../pic/maudio_xponent.png)
 
-    {assert}  = require 'mixco/lib/util'
-    script    = require 'mixco/lib/script'
+    {assert}  = require '../src/util'
+    script    = require '../src/script'
 
-    c = require 'mixco/lib/control'
-    b = require 'mixco/lib/behaviour'
-    v = require 'mixco/lib/value'
+    c = require '../src/control'
+    b = require '../src/behaviour'
+    v = require '../src/value'
 
     ledButtonFixed = ->
         c.ledButton(arguments...).option

spec/mock.litcoffee
@@ -1,109 +1,0 @@
-spec.mock
-=========
-
-*Mocks* for objects provided by the Mixxx context. Please *update them
-when needed*, reflecting [the official Mixxx documentation](
-http://mixxx.org/wiki/doku.php/midi_scripting).
-
-    script = require '../src/script'
-    {assert} = require '../src/util'
-
-Engine
-------
-
-    exports.engine = ->
-        obj = createSpyObj 'engine', [
-            'getValue',       # (group, key)
-            'setValue',       # (group, key, value)
-            'softTakeover',   # (group, key, enable)
-            'beginTimer',     # (delay, handler) -> id
-            'stopTimer',      # (id)
-            'connectControl', # (group, key, handler[, disconnnect])
-            'scratchEnable',  # (int deck, int intervalsPerRev, float rpm,
-                              #  alpha, beta, ramp)
-            'scratchTick',    # (int deck, int interval)
-            'scratchDisable', # (int deck)
-            'isScratching',   # (int deck)
-            'brake',          # (int deck, bool activate[, float factor, rate])
-            'spinback',       # (int deck, bool activate[, float factor, rate])
-            ]
-
-        do ->
-            values = {}
-            obj.getValue.andCallFake (group, key) ->
-                values[[group, key]] ? 0
-            obj.setValue.andCallFake (group, key, value)->
-                values[[group, key]] = value
-
-        obj.connectControl.andCallFake do ->
-            connections = {}
-            (group, key, handler, disconnect) ->
-                id = [group, key, handler]
-                if disconnect
-                    assert id of connections,
-                        "Disconnect not connect control: #{group}, #{key}"
-                    delete connections[id]
-                else
-                    assert id not of connections,
-                        "Connect connected control: #{group}, #{key}"
-                    connections[id] = true
-        obj
-
-Midi
-----
-
-    exports.midi = ->
-        createSpyObj 'engine', [
-            'sendShortMsg', # (status, id, value)
-            'sendSysexMsg', # (data, length)
-            ]
-
-
-Midi
-----
-
-    exports.script = ->
-        createSpyObj "script", [
-            'pitch', # (control, value, status)
-            ]
-
-
-Fake mixxx object
------------------
-
-    exports.mixxx = ->
-        engine: exports.engine()
-        midi:   exports.midi()
-        script: exports.script()
-
-Script
-------
-
-This test script class provides Mixxx object mocks.
-
-    class exports.TestScript extends script.Script
-
-        @property 'mixxx',
-            get: ->
-                @_fakeMixxx or= exports.mixxx()
-
-    exports.testScript = -> new exports.TestScript arguments...
-
-
-License
--------
-
->  Copyright (C) 2013 Juan Pedro Bolívar Puente
->
->  This program is free software: you can redistribute it and/or
->  modify it under the terms of the GNU General Public License as
->  published by the Free Software Foundation, either version 3 of the
->  License, or (at your option) any later version.
->
->  This program is distributed in the hope that it will be useful,
->  but WITHOUT ANY WARRANTY; without even the implied warranty of
->  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
->  GNU General Public License for more details.
->
->  You should have received a copy of the GNU General Public License
->  along with this program.  If not, see <http://www.gnu.org/licenses/>.

test/mixco/control.spec.coffee
@@ -1,12 +1,16 @@
 # spec.mixco.behaviour
 # ====================
 
+chai = {expect} = require 'chai'
+{spy, mock} = require 'sinon'
+chai.use require 'sinon-chai'
+
 describe 'mixco.control', ->
 
     {union} = require 'underscore'
-    mock = require '../mock'
-    c = {MIDI_CC, Control, InControl, OutControl} = require '../../lib/control'
-    {Behaviour} = behaviour = require '../../lib/behaviour'
+    mocks = require '../mock'
+    c = {MIDI_CC, Control, InControl, OutControl} = require '../../src/control'
+    {Behaviour} = behaviour = require '../../src/behaviour'
 
     describe 'Control', ->
 
@@ -16,24 +20,24 @@ describe 'mixco.control', ->
             control = new Control
 
         it "exposes script when initialized", ->
-            script = new mock.TestScript "script"
+            script = new mocks.TestScript "script"
 
             control.init script
-            expect(control.script).toBe(script)
+            expect(control.script).to.eq script
 
             control.shutdown script
-            expect(control.script).not.toBeDefined()
+            expect(control.script).not.to.exist
 
         it "converts number or pair in constructor to CC midi id", ->
             control = new Control 32
-            expect(control.ids.length).toBe(1)
+            expect(control.ids.length).to.eq 1
             {message, midino, channel} = control.ids[0]
-            expect([message, midino, channel]).toEqual [MIDI_CC, 32, 0]
+            expect([message, midino, channel]).to.eql [MIDI_CC, 32, 0]
 
             control = new Control 64, 8
-            expect(control.ids.length).toBe(1)
+            expect(control.ids.length).to.eq 1
             {message, midino, channel} = control.ids[0]
-            expect([message, midino, channel]).toEqual [MIDI_CC, 64, 8]
+            expect([message, midino, channel]).to.eql [MIDI_CC, 64, 8]
 
 
     describe 'InControl', ->
@@ -47,13 +51,13 @@ describe 'mixco.control', ->
             beh1 = new Behaviour
             control.does beh1
             control.option behaviour.option.invert
-            expect(beh1._options).toEqual [behaviour.option.invert]
+            expect(beh1._options).to.eql [behaviour.option.invert]
 
         it "propagates options to its new behaviours", ->
             beh1 = new Behaviour
             control.option behaviour.option.invert
             control.does beh1
-            expect(beh1._options).toEqual [behaviour.option.invert]
+            expect(beh1._options).to.eql [behaviour.option.invert]
 
         it "propagates options to its conditional behaviours", ->
             beh1 = new Behaviour
@@ -62,13 +66,13 @@ describe 'mixco.control', ->
             control.option behaviour.option.invert
 
             control.when new Behaviour, beh1
-            expect(beh1._options).toEqual [behaviour.option.invert]
+            expect(beh1._options).to.eql [behaviour.option.invert]
 
             control.else.when new Behaviour, beh2
-            expect(beh2._options).toEqual [behaviour.option.invert]
+            expect(beh2._options).to.eql [behaviour.option.invert]
 
             control.else beh3
-            expect(beh3._options).toEqual [behaviour.option.invert]
+            expect(beh3._options).to.eql [behaviour.option.invert]
 
         it "configures the options of its behaviour when it can", ->
             beh1 = new Behaviour
@@ -80,7 +84,7 @@ describe 'mixco.control', ->
             control.does beh1
 
             expect(control.configInputs 0)
-                .toMatch ///
+                .to.match ///
                     \s*<options>
                     \s*<invert/>
                     \s*<soft-takeover/>
@@ -93,8 +97,8 @@ describe 'mixco.control', ->
             beh1.option behaviour.option.softTakeover
             control.does beh1
 
-            expect(control.configInputs 0, mock.testScript())
-                .toMatch ///
+            expect(control.configInputs 0, mocks.testScript())
+                .to.match ///
                     \s*<options>
                     \s*<script-binding/>
                     \s*</options>
@@ -113,8 +117,8 @@ describe 'mixco.control', ->
             control.does beh1
             control.does beh2
 
-            expect(control.configInputs 0, mock.testScript())
-                .toMatch ///
+            expect(control.configInputs 0, mocks.testScript())
+                .to.match ///
                     \s*<options>
                     \s*<script-binding/>
                     \s*</options>
@@ -127,8 +131,8 @@ describe 'mixco.control', ->
                 key: "crossfader"
             control.does beh1.option {}
 
-            expect(control.configInputs 0, mock.testScript())
-                .toMatch ///
+            expect(control.configInputs 0, mocks.testScript())
+                .to.match ///
                     \s*<options>
                     \s*<script-binding/>
                     \s*</options>
@@ -141,8 +145,8 @@ describe 'mixco.control', ->
                 key: "crossfader"
             control.does beh1
 
-            expect(control.configInputs 0, mock.testScript())
-                .toMatch ///
+            expect(control.configInputs 0, mocks.testScript())
+                .to.match ///
                     \s*<options>
                     \s*<normal/>
                     \s*</options>
@@ -160,13 +164,13 @@ describe 'mixco.control', ->
                 minimum: 1
                 maximum: 2
             config = control.configOutputs 0
-            expect(config).toContain("<minimum>1</minimum>")
-            expect(config).toContain("<maximum>2</maximum>")
+            expect(config).to.contain("<minimum>1</minimum>")
+            expect(config).to.contain("<maximum>2</maximum>")
 
             behave.directOutMapping = -> {}
             config = control.configOutputs 0
-            expect(config).not.toContain("<minimum>")
-            expect(config).not.toContain("<maximum>")
+            expect(config).not.to.contain("<minimum>")
+            expect(config).not.to.contain("<maximum>")
 
          it "configures an output for every midi id that is not a note off", ->
              control = new OutControl union c.ccIds(0x42), c.noteIds(0x33)
@@ -175,47 +179,47 @@ describe 'mixco.control', ->
 
              behave.directOutMapping = -> {}
              config = control.configOutputs 0
-             expect(config).toMatch ///
+             expect(config).to.match ///
                      \s*<status>0xb0</status>
                      \s*<midino>0x42</midino>
                  ///
-             expect(config).toMatch ///
+             expect(config).to.match ///
                      \s*<status>0x90</status>
                      \s*<midino>0x33</midino>
                  ///
-             expect(config).not.toMatch ///
+             expect(config).not.to.match ///
                      \s*<status>0x80</status>
                      \s*<midino>0x33</midino>
                  ///
 
         it "turns off completely the controls on shut down", ->
-            script = new mock.TestScript "script"
+            script = new mocks.TestScript "script"
             control = new OutControl
-            spyOn(control, 'doSend')
+            spy(control, 'doSend')
 
             control.init script
-            expect(control.doSend).not.toHaveBeenCalledWith 'disable'
+            expect(control.doSend).not.to.have.been.calledWith 'disable'
             control.shutdown script
-            expect(control.doSend).toHaveBeenCalledWith 'disable'
+            expect(control.doSend).to.have.been.calledWith 'disable'
 
         it "sends midi to all outputs that are not a note off", ->
-            script = new mock.TestScript "script"
+            script = new mocks.TestScript "script"
             control = new OutControl union c.ccIds(0x42), c.noteIds(0x33)
 
             control.init script
             control.doSend 'on'
 
             expect(script.mixxx.midi.sendShortMsg)
-                .toHaveBeenCalledWith control.ids[0].status(),
+                .to.have.been.calledWith control.ids[0].status(),
                                       control.ids[0].midino,
                                       0x7f
             expect(script.mixxx.midi.sendShortMsg)
-                .toHaveBeenCalledWith control.ids[1].status(),
+                .to.have.been.calledWith control.ids[1].status(),
                                       control.ids[1].midino,
                                       0x7f
             expect(script.mixxx.midi.sendShortMsg)
                 .not
-                .toHaveBeenCalledWith control.ids[2].status(),
+                .to.have.been.calledWith control.ids[2].status(),
                                       control.ids[2].midino,
                                       0x7f
 

test/mixco/script.spec.coffee
@@ -1,11 +1,15 @@
 # spec.mixco.value
 # ================
 
+chai = {expect} = require 'chai'
+{stub} = require 'sinon'
+chai.use require 'sinon-chai'
+
 describe 'mixco.script', ->
 
     {isinstance} = require 'heterarchy'
-    {Script, register} = require '../../lib/script'
-    control = require '../../lib/control'
+    {Script, register} = require '../../src/script'
+    control = require '../../src/control'
 
     class TestScript extends Script
 
@@ -18,12 +22,12 @@ describe 'mixco.script', ->
 
         it 'configures controller id to be de script name', ->
             expect(script.config())
-                .toMatch "<controller id=\"testscript\">[^$]*</controller>"
+                .to.match /<controller id=\"testscript\">[^$]*<\/controller>/
 
         it 'can generate configuration with partial metadata', ->
             delete script.info.wiki
             expect(script.config())
-                .not.toContain "undefined"
+                .not.to.contain "undefined"
 
     describe 'register', ->
 
@@ -31,38 +35,42 @@ describe 'mixco.script', ->
             testModule = exports: {}
             register testModule, TestScript
             expect(isinstance testModule.exports.testscript, TestScript)
-                .toBe true
+                .to.be.true
 
         it 'can generate a script type from a definition', ->
-            spy = createSpyObj 'scriptSpy', [
-                'constructor', 'preinit', 'init', 'shutdown', 'postshutdown' ]
+            spier = stub
+                constructor: ->
+                preinit: ->
+                init: ->
+                shutdown: ->
+                postshutdown: ->
             testModule = exports: {}
 
             register testModule,
                 name: 'awesome_script'
-                constructor: -> spy.constructor()
+                constructor: -> spier.constructor()
                 preinit: ->
-                    spy.preinit()
-                    expect(@_isInit).not.toBeDefined()
-                init: -> spy.init()
+                    spier.preinit()
+                    expect(@_isInit).not.to.exist
+                init: -> spier.init()
                 postshutdown: ->
-                    spy.postshutdown()
-                    expect(@_isInit).not.toBeDefined()
-                shutdown: -> spy.shutdown()
+                    spier.postshutdown()
+                    expect(@_isInit).not.to.exist
+                shutdown: -> spier.shutdown()
                 info: author: 'Jimmy Jazz'
 
             script = testModule.exports.awesome_script
-            expect(script.name).toBe 'awesome_script'
-            expect(script.info.author).toBe 'Jimmy Jazz'
-            expect(spy.constructor).toHaveBeenCalled()
+            expect(script.name).to.be.eq 'awesome_script'
+            expect(script.info.author).to.be.eq 'Jimmy Jazz'
+            expect(spier.constructor).to.have.been.called
 
             script.init()
-            expect(spy.preinit).toHaveBeenCalled()
-            expect(spy.init).toHaveBeenCalled()
+            expect(spier.preinit).to.have.been.called
+            expect(spier.init).to.have.been.called
 
             script.shutdown()
-            expect(spy.shutdown).toHaveBeenCalled()
-            expect(spy.postshutdown).toHaveBeenCalled()
+            expect(spier.shutdown).to.have.been.called
+            expect(spier.postshutdown).to.have.been.called
 
         it 'controls created during construction are registered autoamtically', ->
             testModule = exports: {}
@@ -75,9 +83,9 @@ describe 'mixco.script', ->
                     expectedControls.push control.ledButton()
 
             expect(expectedControls.length)
-                .toBe 2
+                .to.be.eq 2
             expect(testModule.exports.some_script.controls)
-                .toEqual expectedControls
+                .to.eql expectedControls
 
 # License
 # -------

commit 537fea6b3f208fd68ab3453047bbbe38e592322b

Fix generating the Mixxx scripts
This was broken in:
    c445abb Generate test coverage for the CoffeeScript code

The problem is that now we include the coffee-script original sources
directly in the scripts, such that the code coverage properly works.
Browserify was getting confused, because we did not instruct it how to
deal with coffee-script sources.

Another problem is that latest Browserify would not generate a named
module for the entry point even when the right --require parameter is
passed.  This has been solved:

  https://github.com/substack/node-browserify/issues/1021

makefile
@@ -82,18 +82,38 @@ tmp/%.js: script/%.js
 	@mkdir -p $(@D)
 	cp -f $< $@
 
-out/%.js: tmp/%.js $(FRAMEWORK)
+out/%.js: script/%.js
 	@mkdir -p $(@D)
-	$(BROWSERIFY) -r ./$< $< > $@
-	echo ";$*=require('./$<').$*" >> $@
+	@mkdir -p tmp
+	echo "require('../$<')" >> tmp/$*.entry.js
+	$(BROWSERIFY) \
+		-t coffeeify --extension=".js" --extension=".coffee" --extension=".litcoffee" \
+		-r "./$<:$*" tmp/$*.entry.js -o $@
+	echo ";$*=require('$*').$*" >> $@
+out/%.js: script/%.litcoffee
+	@mkdir -p $(@D)
+	@mkdir -p tmp
+	echo "require('../$<')" >> tmp/$*.entry.js
+	$(BROWSERIFY) \
+		-t coffeeify --extension=".js" --extension=".coffee" --extension=".litcoffee" \
+		-r "./$<:$*" tmp/$*.entry.js -o $@
+	echo ";$*=require('$*').$*" >> $@
+out/%.js: script/%.coffee
+	@mkdir -p $(@D)
+	@mkdir -p tmp
+	echo "require('../$<')" >> tmp/$*.entry.js
+	$(BROWSERIFY) \
+		-t coffeeify --extension=".js" --extension=".coffee" --extension=".litcoffee" \
+		-r "./$<:$*" tmp/$*.entry.js -o $@
+	echo ";$*=require('$*').$*" >> $@
 
-out/%.midi.xml: script/%.litcoffee $(FRAMEWORK)
+out/%.midi.xml: script/%.litcoffee
 	@mkdir -p $(@D)
 	$(COFFEE) $< -g > $@
-out/%.midi.xml: script/%.coffee $(FRAMEWORK)
+out/%.midi.xml: script/%.coffee
 	@mkdir -p $(@D)
 	$(COFFEE) $< -g > $@
-out/%.midi.xml: script/%.js $(FRAMEWORK)
+out/%.midi.xml: script/%.js
 	@mkdir -p $(@D)
 	$(NODEJS) $< -g > $@
 

script/novation_twitch.js
@@ -29,6 +29,7 @@
 // modules are usable too when writing your script with the *Mixco*
 // framework.
 
+require("coffee-script/register")
 var _      = require('underscore')
 var script = require('../src/script')
 var c      = require('../src/control')